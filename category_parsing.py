bb28c149010840158f8e339fb22bfe5214520123
Nagaev Kirill <bmxrider@mail.ru>
2022-11-13 22:43:11 +0300

    first commit

---
+++ category_parsing.py
@@ -0,0 +1,61 @@
+import json
+import requests
+import time
+import ENV
+
+
+def parse_category(cat_name, url, min_discount):
+    response = requests.get(url + '&page=1')
+    data_response = response.json()['data']
+
+    total_pages = data_response['pageTotalCount']
+    product_dict = []
+
+    for i in range(1, total_pages+1):
+        response = requests.get(url=f'{url}&page={i}')
+        data_response = response.json()['data']
+        products = data_response['products']
+
+        for item in products:
+            if int(item['price']['discount']) > min_discount:
+                product_card = {
+                    'id': item['id'],
+                    'name': item['name'],
+                    'link': f"https://divan.ru{item['link']}",
+                    'price': item['price'],
+                    'image': item['images'][0]['src']
+                }
+
+                product_dict.append(product_card)
+
+    # print(product_dict)
+    with open(f'{ENV.DAILY_PATH}/{cat_name}/{cat_name}_{time.strftime("%H%M%S-%d%m%Y")}.json', 'w', encoding='utf-8') as file:
+        json.dump(product_dict, file, indent=4, ensure_ascii=False)
+
+
+def product_mapping(file_source, file_new):
+    with open(f'{ENV.DAILY_PATH}/{file_new}', 'r', encoding='utf-8') as file_temp:
+        temp_file = json.load(file_temp)
+    with open(f'{ENV.DAILY_PATH}/{file_source}', 'r', encoding='utf-8') as file_last:
+        last_file = json.load(file_last)
+
+    id_dict_last = []
+    id_dict_temp = []
+
+    for item in temp_file:
+        id_dict_temp.append(item['id'])
+    for item in last_file:
+        id_dict_last.append(item['id'])
+
+    diff = set(id_dict_temp) - set(id_dict_last)
+    with open(f'{ENV.DAILY_PATH}/default.log', 'a', encoding='utf-8') as file_log:
+        file_log.write(time.strftime("%H%M%S-%d%m%Y")+'\n')
+        if str(diff) == 'set()':
+            file_log.write('There is no new goods \n')
+
+        else:
+            file_log.write(str(diff) + '\n')
+
+
+parse_category('divany-i-kresla', 'https://proxy.divan.ru/p?u=/category/get-products?slug=divany-i-kresla&isInit=true', 0)
+parse_category('krovati-i-matrasy', 'https://proxy.divan.ru/p?u=/category/get-products?slug=krovati-i-matrasy&isInit=true', 0)
